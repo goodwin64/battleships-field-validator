c055df23395265395a013829b0ba4144bb691407
Max Donchenko <wereya2@gmail.com>
2016-02-24 15:00:33 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def check_outer_perimeter(field, ioffset, joffset,
+                          ship_direction):
+    pass
+
+
+def get_direction(field, ioffset, joffset):
+    pass
+
+
+def validate_battlefield(field):
+    # some checks
+    return True
